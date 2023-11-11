ee6e2fcb8595465171d39e1a322b6f5f7d29b74b
Robert Arcaleanu <alumnerobert@gmail.com>
2023-11-11 18:39:51 +0100

    functions to extract, transform and load

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import requests
 import os
 
-APP_ID = os.getenv('APP_ID_AMS')
-APP_KEY = os.getenv('APP_KEY_AMS')
-
 URL = 'https://api.schiphol.nl/public-flights/'
 ENDPOINT = 'flights'
 
-header = {
-    'Accept': 'application/json',
-    'app_id': APP_ID,
-    'app_key': APP_KEY,
-    'ResourceVersion': 'v4'
-}
+APP_ID = os.getenv('APP_ID_AMS')
+APP_KEY = os.getenv('APP_KEY_AMS')
 
-params = {
-    'scheduleDate': '2023-11-08',
-    'page': 1
-}
+def extract(URL, ENDPOINT, page, APP_ID, APP_KEY):
+    """
+    This function extracts the data from the API
+    """
 
-response = requests.get(url=URL + ENDPOINT, headers=header, params=params).json()
-len(response['flights'])
+    header = {
+        'Accept': 'application/json',
+        'app_id': APP_ID,
+        'app_key': APP_KEY,
+        'ResourceVersion': 'v4'
+    }
+
+    params = {
+        'scheduleDate': '2023-11-08',
+        'page': page
+    }
+
+    response = requests.get(url=URL + ENDPOINT, headers=header, params=params).json()
+    return response['flights']
+    
+def transform(data):
+    """
+    This function transforms the data
+    """
+
+    transformed_data = data
+
+    return transformed_data
+
+def load(data):
+    """
+    This function loads the transformed data into a database
+    """
