f884904a664fbe9ae46f96a5a9aefae9519856e8
Robert Arcaleanu <alumnerobert@gmail.com>
2024-03-02 20:00:00 +0100

    created loop to extract all data

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,21 +1,51 @@
 import requests
+import time
+import polars as pl
 
-def extract(URL, ENDPOINT, page, APP_ID, APP_KEY):
+
+from pipeline.transform import transform
+
+def extract_page(URL: str, ENDPOINT: str, page: int, header: dict, fl_dir: str) -> dict:
     """
     This function extracts the data from the API
     """
+
+    params = {
+        'scheduleDate': '2023-11-08',
+        'page': page,
+        'flightDirection': fl_dir
+    }
+
+    response = requests.get(url=URL + ENDPOINT, headers=header, params=params).json()
+    return response['flights']
+
+
+def extract_all(URL: str, ENDPOINT: str, APP_ID: str, APP_KEY: str) -> pl.DataFrame:
 
     header = {
         'Accept': 'application/json',
         'app_id': APP_ID,
         'app_key': APP_KEY,
         'ResourceVersion': 'v4'
-    }
+        }
 
-    params = {
-        'scheduleDate': '2023-11-08',
-        'page': page
-    }
+    data_flights = pl.DataFrame()
+    for dir in ['D', 'A']:
+        page = 0
+        cond = True
+        while cond:
+            try:
+                data_page = extract_page(URL, ENDPOINT, page, header, fl_dir=dir)
+                data_page = pl.DataFrame(data_page)
+                data_page = transform(data_page, fl_dir=dir)
+                data_flights = pl.concat([data_flights, data_page])
+                page = page + 1
+                print(page)
+                time.sleep(1)
 
-    response = requests.get(url=URL + ENDPOINT, headers=header, params=params).json()
-    return response['flights']
+            except:
+                print('Finished at page ' + str(page))
+                break
+            
+
+    return data_flights
--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,12 +1,22 @@
 import polars as pl
 
-def transform(raw_data):
-    """
-    This function transforms the data
+def transform(raw_data: pl.DataFrame, fl_dir: str) -> pl.DataFrame:
+    """Transform the data removing columns and adding new ones.
+
+    Args:
+        raw_data (pl.DataFrame): data obtained fro the API
+        fl_dir (str): Indicates whether the commercial flight is a departure flight (d) or an arrival flight (a)
+
+    Returns:
+        pl.DataFrame: simplified dataframe
     """
 
-    transformed_data = pl.DataFrame(raw_data)
-
+    transformed_data = raw_data.clone()
+    if fl_dir == 'A':
+        time_var = "actualLandingTime" # Actual landing time of an arrival
+    elif fl_dir == 'D':
+        time_var = "actualOffBlockTime" # The actual time of departure of a flight from Amsterdam Airport Schiphol.
+    
     KEEP_COLUMNS = ["id",
                     "flightDirection", # Arrival or Departure
                     "serviceType",
@@ -15,19 +25,15 @@ def transform(raw_data):
                     "scheduleTime", # The time the commercial flight is scheduled to depart or arrive. Example: 15:25:00
                     # "publicFlightState",
                     "aircraftType",
-                    "actualLandingTime", # Actual landing time of an arrival
-                    "actualOffBlockTime", # The actual time of departure of a flight from Amsterdam Airport Schiphol.
-                    "prefixIATA"]
+                    time_var,
+                    "prefixIATA",
+                    "terminal"]
     
     transformed_data = transformed_data.select(KEEP_COLUMNS)
     transformed_data = transformed_data.with_columns(scheduleDateTime = pl.col("scheduleDate") +  " " + pl.col("scheduleTime"))
-    transformed_data = transformed_data.with_columns(
-        pl.when(pl.col("flightDirection") == "A")
-        .then(pl.col("actualLandingTime"))
-        .otherwise(pl.col("actualOffBlockTime"))
-        .alias("actualDateTime")
-    )
-    transformed_data = transformed_data.drop(["scheduleDate", "scheduleTime", "actualLandingTime", "actualOffBlockTime"])
+
+    transformed_data = transformed_data.rename({time_var: 'actualDateTime'})
+    transformed_data = transformed_data.drop(["scheduleDate", "scheduleTime"])
 
     # select aircraftType
 
